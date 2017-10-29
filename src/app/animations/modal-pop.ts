dfb6248fccbb631686029334552f7c08baade7b6
David Reese <dave@freshconsulting.com>
2017-10-29 15:22:45 -0700

    login improvements

--- src/app/animations/modal-pop.ts
+++ src/app/animations/modal-pop.ts
@@ -4,10 +4,10 @@ export const modalPop = trigger('modalPop', [
   state('in', style({transform: 'translate(-50%, -50%)', opacity: '1'})),
   transition(':enter', [
     style({transform: 'translate(-50%, -20%)', opacity: '0'}),
-    animate('0.5s cubic-bezier(.54,0,.03,1)')
+    animate('0.5s cubic-bezier(0.54,0,0.03,1)')
   ]),
   transition(':leave', [
     style({transform: 'translate(-50%, -50%)'}),
-    animate('0.3s cubic-bezier(.54,0,.03,1)', style({transform: 'translate(-50%, -80%)', opacity: '0'}))
+    animate('0.3s cubic-bezier(0.54,0,0.03,1)', style({transform: 'translate(-50%, -80%)', opacity: '0'}))
   ])
 ]);
--- src/app/animations/route-fades.ts
+++ src/app/animations/route-fades.ts
@@ -5,11 +5,11 @@ export const routeFades = trigger('routeFades', [
     group([
       query(':enter', [
         style({opacity: '0'}),
-        animate('0.5s 0.8s cubic-bezier(.54,0,.03,1)', style({opacity: '1'}))
+        animate('400ms 100ms cubic-bezier(.54,0,.03,1)', style({opacity: '1'}))
       ], { optional: true }),
       query(':leave', [
         style({opacity: '1', position: 'absolute', top: '0', left: '0', right: '0'}),
-        animate('0.5s cubic-bezier(.54,0,.03,1)', style({opacity: '0', position: 'absolute', top: '0', left: '0', right: '0'}))
+        animate('400ms cubic-bezier(.54,0,.03,1)', style({opacity: '0', position: 'absolute', top: '0', left: '0', right: '0'}))
       ], { optional: true }),
     ])
   ])
